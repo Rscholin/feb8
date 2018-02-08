83955b4d254dde2feacd137a79dd16a8e3551aa2
Rye Scholin <rye211@MMs-Mac-mini.local>
2018-02-08 10:50:18 -0800

    first

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
-print('hello world')
+
+def readFile():
+    data = open('data.txt','r')
+    readdata = data.read()
+    data.close()
+    return readdata
+
+def printContent(meow):
+    print(meow)
+
+if(__name__ == '__main__'):
+    print('hello world')
+    fileResults = readFile()
+    printContent(fileResults)
+
+
